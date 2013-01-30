87d91570e4a64a07eb13055f515e0ec74c4484d6
Wong Liang Zan <zan@liangzan.net>
2013-01-30 17:11:45 +0800

    stubs for the swift swauth client

--- lib/paperclip/swift/swauth_client.rb
+++ lib/paperclip/swift/swauth_client.rb
@@ -1,6 +1,18 @@
 module Paperclip
   module Swift
     class SwauthClient
+
+      def object_exists?(path)
+      end
+
+      def create_object(path, opts, file)
+      end
+
+      def delete_object(path)
+      end
+
+      def object(path)
+      end
     end
   end
 end
