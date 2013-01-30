c6b0ac42435448d3e1cb9a4bf3b3f98d122d936b
Wong Liang Zan <zan@liangzan.net>
2013-01-30 16:30:07 +0800

    added a stub spec for swauth client

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 require 'rspec'
 require 'shoulda'
 require 'paperclip-swift-swauth'
+
+class Dummy
+  include Paperclip::Storage::SwiftSwauth
+end
--- spec/swift_swauth_spec.rb
+++ spec/swift_swauth_spec.rb
@@ -1,5 +1,51 @@
 require 'spec_helper'
 
-describe Paperclip::Storage::SwiftSwauth do
-  it { true.should be_true }
+describe 'SwiftSwauth' do
+  let(:store) { Dummy.new }
+
+  describe '#exists?' do
+    subject { store.exists?(style) }
+    let(:style) { 'square' }
+
+    context 'when attachment with default style exists' do
+      before { Paperclip::Swift::SwauthClient.any_instance.should_receive(:object_exists?) { true } }
+      it { should be_true }
+    end
+
+    context 'when attachment with defined style exists' do
+      before { Paperclip::Swift::SwauthClient.any_instance.should_receive(:object_exists?).with(style) { true } }
+      it { should be_true }
+    end
+
+    context 'when attachment with default style does not exist' do
+      before { Paperclip::Swift::SwauthClient.any_instance.should_receive(:object_exists?) { false } }
+      it { should be_false }
+    end
+
+    context 'when attachment with defined style does not exist' do
+      before { Paperclip::Swift::SwauthClient.any_instance.should_receive(:object_exists?).with(style) { false } }
+      it { should be_false }
+    end
+  end
+
+  describe '#flush_writes' do
+    subject { store.flush_writes }
+
+    context 'when write queue has items'
+    context 'when write queue has no items'
+  end
+
+  describe '#flush_deletes' do
+    subject { store.flush_deletes }
+
+    context 'when delete queue has items'
+    context 'when delete queue has no items'
+  end
+
+  describe '#copy_to_local_file' do
+    subject { store.copy_to_local_file(style, local_dest_path) }
+
+    context 'when file exists'
+    context 'when file does not exist'
+  end
 end
