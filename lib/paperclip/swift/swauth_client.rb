b61d2775b8f2ebaa31e994191704c349ef6e013c
Wong Liang Zan <zan@liangzan.net>
2013-02-01 14:26:13 +0800

    completed the implementation and specs for the swauth client that works with the dropmyemail fork of openstack

--- lib/paperclip/swift/swauth_client.rb
+++ lib/paperclip/swift/swauth_client.rb
@@ -1,17 +1,54 @@
+require 'openstack'
+
 module Paperclip
   module Swift
     class SwauthClient
+      SERVICE_TYPE = 'object-store'
 
-      def object_exists?(path)
+      def initialize
+        @swift_opts = {
+          user:      ENV['swift_user'] || '',
+          password:  ENV['swift_password'] || '',
+          url:       ENV['swift_url'] || '',
+          tenant:    ENV['swift_tenant'] || '',
+          container: ENV['swift_container'] || ''
+        }
       end
 
-      def create_object(path, opts, file)
+      def object_exists?(path)
+        container.object_exists? path
+      end
+
+      def create_object(path, opts={}, data)
+        container.create_object path, opts, data
       end
 
       def delete_object(path)
+        container.delete_object path
       end
 
       def object(path)
+        container.object path
+      end
+
+      protected
+
+      def container
+        if @connection.nil?
+          @connection = OpenStack::Connection.create(connection_opts)
+        end
+        @connection.container(@swift_opts[:container])
+      end
+
+      def connection_opts
+        {
+          username: @swift_opts[:user],
+          api_key: @swift_opts[:password],
+          auth_url: @swift_opts[:url],
+          authtenant_name: @swift_opts[:tenant],
+          auth_method: @swift_opts[:password],
+          service_type: SERVICE_TYPE
+        }
       end
     end
   end
